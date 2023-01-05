e2890d4596fc696f34f7d8219b177d22676e2e6f
Neider Hernández <neiderkk@gmail.com>
2023-01-05 18:41:53 -0500

    Agregando estilos a mi aplicacion GifApp

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -4,7 +4,13 @@ import React from 'react'
 export const GifExpertApp = () => {
   return (
     <>
+        {/* Title */}
         <h1>GifExpertApp</h1>
+    
+        {/* Input */}
+
+        {/* Gif List */}
+            {/* Gif Item */}
     </>
   )
 }
