d94dd8b4956e49eccc15a11f7f5cc143db093a4d
Neider Hernández <neiderkk@gmail.com>
2023-01-05 19:04:09 -0500

    Creando lista de categorias a mi aplicacion GifApp utilizando Hooks

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,11 @@
 
 import React from 'react'
-
+import { useState } from 'react';
 export const GifExpertApp = () => {
+
+    /* Hooks */
+    const [ categories, setCategories ] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
+    console.log(categories);
   return (
     <>
         {/* Title */}
@@ -10,6 +14,14 @@ export const GifExpertApp = () => {
         {/* Input */}
 
         {/* Gif List */}
+        <ol>
+            { categories.map( category => {
+                return <li key={category}>{category}</li>
+            })}
+            {/* <li> </li> */}
+
+        </ol>
+
             {/* Gif Item */}
     </>
   )
