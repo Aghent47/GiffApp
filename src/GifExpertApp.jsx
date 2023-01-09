fec63f72e7542efe9b16acdf3e46b59499664c9d
Neider Hernández <neiderkk@gmail.com>
2023-01-09 07:10:08 -0500

    Fetch API - Obtener las imágenes deseadas

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -5,7 +5,7 @@ import { GifGrid } from './components/GifGrid';
 export const GifExpertApp = () => {
 
     /* Hooks */
-    const [ categories, setCategories ] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
+    const [ categories, setCategories ] = useState(['One Punch']);
 
     const onAddCategory = ( newCategory ) => {
         if(categories.includes(newCategory))return;
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,14 @@
+export const getGifs = async(category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=fzIAGwFK3oQbgSLwGP998Jh3OFZT2xPT&q=${category}&limit=10`;
+    const result = await fetch(url);
+    const {data} =  await result.json();
+    
+    const gifs = data.map(img => ({
+      id: img.id,
+      title: img.title,
+      url: img.images.downsized_medium.url
+    }));
+  
+    console.log(gifs);
+    return(gifs)
+  }
