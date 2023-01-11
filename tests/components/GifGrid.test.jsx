46b7a02c3b8cd50ab160f80152d0625990f41cf6
Neider Hernández <neiderkk@gmail.com>
2023-01-11 06:15:06 -0500

    Hacer un mock completo de un Custom Hook

--- tests/components/GifGrid.test.jsx
+++ tests/components/GifGrid.test.jsx
@@ -1,9 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import { GifGrid } from "../../src/components/GifGrid";
+import { useFetchGifs } from "../../src/hooks/useFetchGifs";
+
+jest.mock('../../src/hooks/useFetchGifs');
 
 describe('pruebas en el componente <GifGrid/>', () => { 
   
   test('debe mostrar el loadinf inicialmente', () => { 
+
+    useFetchGifs.mockReturnValue({
+      images: [],
+      isLoading: true
+    });
 
     const category = 'One Punch';
     render( <GifGrid category = { category } /> );
@@ -12,6 +20,27 @@ describe('pruebas en el componente <GifGrid/>', () => {
    });
 
    test('debe mostrar los Item cuando se cargan las imagenes useFechGifs ', () => { 
-    
+    const category = 'One Punch';
+    const gifs = [{
+      id: 'ABC',
+      url: 'https://localhost/cualquier/SaitamaSensei.jpg',
+      title: 'Saitama'
+    },{
+      id: 'AB22',
+      url: 'https://localhost/cualquier/goku.jpg',
+      title: 'goku'
+    }
+  ]
+    useFetchGifs.mockReturnValue({
+      images: gifs,
+      isLoading: false
+    });
+
+    render( <GifGrid category = { category } /> );
+    /* screen.debug(); */
+
+    expect(screen.getAllByRole('img').length).toBe(gifs.length);
+
+
   });
 });
